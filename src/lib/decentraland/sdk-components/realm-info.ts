f9cd5083ad21bbeae2e1f8f7b3970571b9f89a05
Gabriel Diaz <gabrieldiaz31@gmail.com>
2026-01-13 16:21:37 -0300

    styles: Fix import order

--- src/lib/decentraland/sdk-components/realm-info.ts
+++ src/lib/decentraland/sdk-components/realm-info.ts
@@ -1,4 +1,4 @@
-import { declareComponentUsingProtobufJs } from './pb-based-component-helper'
 import { PBRealmInfo } from '@dcl/protocol/out-js/decentraland/sdk/components/realm_info.gen'
+import { declareComponentUsingProtobufJs } from './pb-based-component-helper'
 
 export const realmInfoComponent = declareComponentUsingProtobufJs(PBRealmInfo, 1106, () => void 0)
